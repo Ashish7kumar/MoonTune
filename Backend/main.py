90b3acf9dfe4be2385e39d13b41ccef3490583ed
unknown <ashish11to12@gmail.com>
2025-08-30 15:42:33 +0530

    added contner logic using modal

---
+++ Backend/main.py
@@ -0,0 +1,10 @@
+import modal
+app = modal.App("moon-tune")
+image = (
+    modal.Image.debian_slim()
+    .apt_install("git")
+    . pip_install_from_requirements("requirements.txt")
+    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step", "cd /tmp/ACE-Step && pip install ."])
+    .env({"HF_HOME": "/.cache/huggingface"})
+    .add_local_python_source("prompts")
+)
